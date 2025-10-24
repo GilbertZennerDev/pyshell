a8d850c77fa54eea77136bc0d8b932c94201f592
gzenner <gzenner@student.42luxembourg.lu>
2025-10-24 09:27:17 +0200

    tets

--- pyshell.py
+++ pyshell.py
@@ -15,7 +15,7 @@ def error(e):
 	print(e)
 
 def pwd():
-	os.getcwd()
+	print(os.getcwd())
 def cd(newdir):
 	try: os.chdir(os.path.expanduser(newdir))
 	except Exception as e: error(e)
@@ -23,10 +23,14 @@ def runprog(*args):
 	try: sp.run(*args)
 	except Exception as e: error(e)
 def run():
+	cmds = ['pwd', 'cd', 'export', 'echo']
 	while 1:
 		txt = input("pyshell: ").split(' ')
 		if txt[0] == 'pwd': pwd()
-		if txt[0] == 'cd':
-			if txt[1]: cd(txt[1])
-		else: runprog(txt)
+		if txt[0] == 'cd' and txt[1]: cd(txt[1])
+		if '$' in txt[0]: print(os.getenv(txt[0]))
+		#if txt[0] == 'export': runprog(['bash', '-c', " ".join(txt)]); print(" ".join(txt))
+		if txt[0] == 'echo':
+			if txt[1][0] == '$': txt[1] = txt[1][1:]; print(os.getenv(txt[1]))
+		if txt[0] not in cmds: runprog(txt)
 run()
