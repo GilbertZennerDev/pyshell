079efebe6f16e5f8d40f45b9aabf88e65cea4284
gzenner <gzenner@student.42luxembourg.lu>
2025-10-24 08:53:56 +0200

    pwd,cd,sp

--- pyshell.py
+++ pyshell.py
@@ -1,3 +1,32 @@
 '''
 a shell in python
+
+supported commands:
+
+cd
+pwd
+./a.out runs program
 '''
+
+import os
+import subprocess as sp
+
+def error(e):
+	print(e)
+
+def pwd():
+	os.getcwd()
+def cd(newdir):
+	try: os.chdir(os.path.expanduser(newdir))
+	except Exception as e: error(e)
+def runprog(*args):
+	try: sp.run(*args)
+	except Exception as e: error(e)
+def run():
+	while 1:
+		txt = input("pyshell: ").split(' ')
+		if txt[0] == 'pwd': pwd()
+		if txt[0] == 'cd':
+			if txt[1]: cd(txt[1])
+		else: runprog(txt)
+run()
